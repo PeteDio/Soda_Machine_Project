bfb6d8b42302900a48342ac624e753d7f5bde54a
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-10-13 14:03:45 -0500

    finished tests for the returning coin

--- test_customer.py
+++ test_customer.py
@@ -11,6 +11,21 @@ class TestGetWalletCoin(unittest.TestCase):
         """Pass in 'Quarter', method should return a Quarter instance"""
         returned_coin = self.customer.get_wallet_coin('Quarter')
         self.assertEqual(returned_coin.value, .25)
+    
+    def test_can_return_penny(self):
+        """Pass in 'Penny', method should return a Penny instance"""
+        returned_coin = self.customer.get_wallet_coin('Penny')
+        self.assertEqual(returned_coin.value, .01)
+
+    def test_can_return_dime(self):
+        """Pass in 'Dime', method should return a Dime instance"""
+        returned_coin = self.customer.get_wallet_coin('Dime')
+        self.assertEqual(returned_coin.value, .1)
+
+    def test_can_return_nickle(self):
+        """Pass in 'Nickel', method should return a Nickel instance"""
+        returned_coin = self.customer.get_wallet_coin('Nickel')
+        self.assertEqual(returned_coin.value, .05)
 
 if __name__ == '__main__':
     unittest.main()
