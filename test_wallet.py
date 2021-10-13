4caf7a2de5e9b3afdd4d57182e457f6a9eb23ca9
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-10-13 18:23:14 -0500

    Stopping point for the day

--- test_wallet.py
+++ test_wallet.py
@@ -8,3 +8,6 @@ class TestWallet(unittest.TestCase):
     def test_fill_wallet(self):
         self.assertEqual(len(self.my_wallet.money), 88)
         print(len(self.my_wallet.money))
+
+if __name__ == '__main__':
+    unittest.main()        
