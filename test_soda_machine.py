d64521184ac49be31686823492f079dd07a6a9ad
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-10-14 15:21:48 -0500

    Finished the TestGetInventorySoda class everything looks good.

--- test_soda_machine.py
+++ test_soda_machine.py
@@ -1,5 +1,5 @@
 import unittest
-from cans import Cola
+from cans import Cola, OrangeSoda, RootBeer
 from customer import Customer
 from coins import Quarter, Dime, Nickel, Penny
 from soda_machine import SodaMachine
@@ -57,26 +57,26 @@ class TestRegisterHasCoin(unittest.TestCase):
 
     def test_register_has_quarter(self):
         """Tests if register has quarters"""
-        self.quarter = Quarter()
-        returned_coin = self.my_soda_machine.register_has_coin(self.quarter.name)
+        quarter = Quarter()
+        returned_coin = self.my_soda_machine.register_has_coin(quarter.name)
         self.assertTrue(returned_coin)
 
     def test_register_has_dime(self):
         """Tests if register has dimes"""
-        self.dime = Dime()
-        returned_coin = self.my_soda_machine.register_has_coin(self.dime.name)
+        dime = Dime()
+        returned_coin = self.my_soda_machine.register_has_coin(dime.name)
         self.assertTrue(returned_coin)
 
     def test_register_has_nickel(self):
         """Tests if register has nickels"""
-        self.nickel = Nickel()
-        returned_coin = self.my_soda_machine.register_has_coin(self.nickel.name)
+        nickel = Nickel()
+        returned_coin = self.my_soda_machine.register_has_coin(nickel.name)
         self.assertTrue(returned_coin)
 
     def test_register_has_penny(self):
         """Tests if register has pennies"""
-        self.penny = Penny()
-        returned_coin = self.my_soda_machine.register_has_coin(self.penny.name)
+        penny = Penny()
+        returned_coin = self.my_soda_machine.register_has_coin(penny.name)
         self.assertTrue(returned_coin)
 
     def test_register_invalid_coin(self):
@@ -103,7 +103,54 @@ class TestDetermineChangeValue(unittest.TestCase):
         changed_value = self.my_soda_machine.determine_change_value(1.00, 1.00)
         self.assertEqual(changed_value, 0)
         
+class TestCalculateCoinValue(unittest.TestCase):
+    def setUp(self):
+        self.my_soda_machine = SodaMachine()
 
+    def test_calculate_coin_value(self):
+        """Passing through each type of coin and making sure the value is correct"""
+        quarter = Quarter()
+        nickel = Nickel()
+        dime = Dime()
+        penny = Penny()
+        
+
+        self.money_list = []
+        self.money_list.append(quarter)
+        self.money_list.append(nickel)
+        self.money_list.append(dime)
+        self.money_list.append(penny)
+
+        
+
+        self.assertEqual(self.my_soda_machine.calculate_coin_value(self.money_list), .41)
+
+    def test_calculate_coin_value_with_zero(self):
+        """passing through nothing should return 0"""
+        self.money_list = []
+        self.assertEqual(self.my_soda_machine.calculate_coin_value(self.money_list), 0)        
+
+
+class TestGetInventorySoda(unittest.TestCase):
+    def setUp(self):
+        self.my_soda_machine = SodaMachine()
+
+    def test_get_inventory_soda_a(self):
+        """taking the name property and checking it against it's actual name"""
+        cola = Cola()
+        orange_soda = OrangeSoda()
+        root_beer = RootBeer()
+        self.soda_one = self.my_soda_machine.get_inventory_soda(cola.name)
+        self.soda_two = self.my_soda_machine.get_inventory_soda(orange_soda.name)
+        self.soda_three = self.my_soda_machine.get_inventory_soda(root_beer.name)
+        self.assertEqual(self.soda_one.name, "Cola")
+        self.assertEqual(self.soda_two.name, "Orange Soda")
+        self.assertEqual(self.soda_three.name, "Root Beer")
+        
+    def test_get_inventory_soda_b(self):
+        """Sending in a string and checking against other names should return None"""
+        self.soda_one = self.my_soda_machine.get_inventory_soda('Mountain Deu')
+        self.assertIsNone(self.soda_one)
 
 if __name__ == '__main__':
     unittest.main()        
