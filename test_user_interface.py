41dd0b08c0b030b946eaa595e5623e07fdf8ae22
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-10-18 14:10:57 -0500

    updated and finished the rest of the test for the TestUserInterfaceClass

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,13 +1,35 @@
 import unittest
-import user_interface
+import user_interface 
 from cans import Cola, OrangeSoda, RootBeer
 from coins import Quarter, Dime, Nickel, Penny
+
 class TestUserInterface(unittest.TestCase):
     
     def test_validate_main_menu(self):
-        """Validating if passing 1 tupple (True, number) will be returned"""
+        """Validating if passing 1, tupple (True, number) will be returned"""
         returned_value = user_interface.validate_main_menu(1)
         self.assertEqual(returned_value, (True, 1))
+
+    def test_validate_main_menu_two(self):
+        """Validating if passing 2, tupple (True, number) will be returned"""
+        returned_value = user_interface.validate_main_menu(2)
+        self.assertEqual(returned_value, (True, 2))
+
+    def test_validate_main_menu_three(self):
+        """Validating if passing 3, tupple (True, number) will be returned"""
+        returned_value = user_interface.validate_main_menu(3)
+        self.assertEqual(returned_value, (True, 3))
+
+    def test_validate_main_menu_four(self):
+        """Validating if passing 4, tupple (True, number) will be returned"""
+        returned_value = user_interface.validate_main_menu(4)
+        self.assertEqual(returned_value, (True, 4))    
+
+    def test_validate_main_menu_b(self):
+        """Validating if passing 5, fapple (False, None) will be returned"""
+        returned_value = user_interface.validate_main_menu(5)
+        self.assertEqual(returned_value, (False, None)) 
+
 
 if __name__ == "__main__":
     unittest.main()        
