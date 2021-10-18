b970348d96f60c15d536ad1c42a6c89790e52704
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-10-18 14:04:41 -0500

    Quick little stopping point added the unittest.main and so far 1 test works.

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,10 +1,13 @@
 import unittest
 import user_interface
 from cans import Cola, OrangeSoda, RootBeer
-from coins import Quarter, Dime, Nickel
+from coins import Quarter, Dime, Nickel, Penny
 class TestUserInterface(unittest.TestCase):
     
     def test_validate_main_menu(self):
         """Validating if passing 1 tupple (True, number) will be returned"""
         returned_value = user_interface.validate_main_menu(1)
         self.assertEqual(returned_value, (True, 1))
+
+if __name__ == "__main__":
+    unittest.main()        
